37db44edbc0f1794e3d1c47fba4c26fdf0fd9e8a
masudRana88 <mahinuddin88@gmail.com>
2022-07-29 18:10:39 +0600

    Navbar shadow is added

--- src/components/container/NavBar/NavBar.js
+++ src/components/container/NavBar/NavBar.js
@@ -3,9 +3,9 @@ import { HiMenu } from "react-icons/hi";
 const NavBar = () => {
     return (
         // <!-- Navbar -->
-        <div className="fixed z-50 w-full bg-bgMain ">
+        <div className="fixed z-50 w-full shadow-lg bg-bgMain">
         <div className="container flex items-start justify-between w-full p-5 mx-auto md:flex-row ">
-          <a href='#!' className="text-lg  md:mb-0 md:text-2xl">
+          <a href='#!' className="text-lg md:mb-0 md:text-2xl">
           Abacus Convention Center
           </a>
           <nav className="items-center justify-center hidden text-base md:ml-auto md:flex">
@@ -18,7 +18,7 @@ const NavBar = () => {
              <HiMenu className='p-1 text-4xl'/>
           </button>
 
-          <div className="offcanvas offcanvas-end fixed bottom-0 flex flex-col max-w-[50%] bg-white invisible bg-clip-padding shadow-sm outline-none transition duration-300 ease-in-out text-gray-700 top-0 right-0 border-none w-96" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+          <div className="offcanvas offcanvas-end fixed bottom-0 flex flex-col max-w-[50%] bg-bgMain invisible bg-clip-padding shadow-sm outline-none transition duration-300 ease-in-out text-gray-700 top-0 right-0 border-none w-96" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
             <div className="flex items-center justify-between p-4 offcanvas-header">
                 <button type="button" className="box-content w-4 h-4 p-2 text-black border-none rounded-none opacity-50 btn-close focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
